1b4879c5c80f6b6dc075cebeffa036b77c94dd59
Wang <415892223@qq.com>
2019-12-24 18:09:55 +0800

    关键字驱动，读取excel表格

--- keyword_model/keyword_actionMethod.py
+++ keyword_model/keyword_actionMethod.py
@@ -13,11 +13,9 @@ from base.find_element import FindElement
 
 class ActionMethod(object):
 
-    def __init__(self):
-        pass
-
     # 打开浏览器
     def open_browser(self, browser):
+        print('打开浏览器')
         if browser == 'chrome':
             self.driver = webdriver.Chrome()
         elif browser == 'firefox':
@@ -27,18 +25,21 @@ class ActionMethod(object):
 
     # 输入地址
     def get_url(self, url):
+        print('输入地址')
         self.driver.get(url)
 
     # 定位元素
     def get_element(self, key):
         # 获取元素
+        print('获取元素', key)
+        # print(self.driver)
         find_element = FindElement(self.driver)
         element = find_element.get_element(key)
         return element
 
     # 输入元素
-    def input_element(self, ket, value):
-        element = self.get_element(ket)
+    def input_element(self, value, key):
+        element = self.get_element(key)
         element.send_keys(value)
 
     # 点击元素
@@ -50,5 +51,15 @@ class ActionMethod(object):
         time.sleep(3)
 
     # 关闭浏览器
-    def close_browser(self):
+    def close_browser(self ):
         self.driver.close()
+
+
+if __name__ == '__main__':
+    am = ActionMethod()
+    am.open_browser('chrome')
+    am.get_url('http://www.5itest.cn/register')
+    # am.get_element('register_email')
+    am.input_element('register_email', '243@qq.com')
+    am.sleep_time()
+    am.close_browser()
